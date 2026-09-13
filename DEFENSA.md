ef6d5a6e1a425d908e3a66f33eb95790194d5f11
José <joseaviles218@gmail.com>
2026-09-13 11:41:09 -0600

    cambios finales

--- DEFENSA.md
+++ DEFENSA.md
@@ -1,22 +1,42 @@
 # laboratorio1_asistencia
 
-A new Flutter project.
+## Estado y ubicación
+El estado se encuentra ubicado en _MyHomePageState y se presenta como late list<Alumno> alumnos; Esta ahí debido a que es el único widget que necesita recordar y tener acceso a los cambios de asistencia de los alumnos y para poder hacer un contador de los presentes.
 
-## Getting Started
+Si en se hubiera ubicado en otra parte, por ejemplo como estado dentro de la TarjetaAlmno, el contador no se actualizaría. El número de alumnos presentes se calcula recorriendo la lista de alumnos y se encontrara en este nuevo lugar, la lista no se actualizaría.
 
-This project is a starting point for a Flutter application.
+## Reconstrucción de widgets
 
-A few resources to get you started if this is your first Flutter project:
+Al momento de realizar una registro de asitencia, se reconstruye el build() de _MyHomePageState porque dentro de este método se encuentra el setState que se utiliza para cambiar los estados de los alumnos. No se hizo nada para que no se reconstruyera de más.
 
-- [Learn Flutter](https://docs.flutter.dev/get-started/learn-flutter)
-- [Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Flutter learning resources](https://docs.flutter.dev/reference/learning-resources)
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Extracción de componente
+
+Se extrajo TarjetaAlumno que originalmente se había puesto dentro de _MyHomePageState. El criterio por el que se extrajo fue que se necesitaba una clase que se podía reutilizar en otros lugares y al ser 12 registros de alumnos, era necesario que se pudiera reutilizar.
+
+Si se hubiera usado un método en lugar de una clase, TarjetaAlumno seguiría dependiendo de _MyHomePageState y no podría ser reutilizado. Por ejemplo, si en una futura expansión se quisiera utilizar en una nueva pantalla, habría que copiar y crear una nueva clase en lugar de utilizar una clase existente.
+
 
 ## USO DE IA
 - Se utilizó para conocer el uso de copyWith() en Flutter. Este método se mencionaba en la guía de diseño de material de flutter.
 1. "¿para que es copywith?"
+
+- Se utilizó para la explicación de errores: "Ten en consideracion lo de ia y explica errores:"
+
+1. Error de state.build:
+Missing concrete implementation of 'State.build'. Try implementing the missing method, or make the class abstract.
+
+2. Error relacionado con el método build:
+The body might complete normally, causing 'null' to be returned, but the return type, 'Widget', is a potentially non-nullable type. Try adding either a return or a throw statement at the end.
+
+3. Error relacionado con TarjetaAlumno:
+The method 'TarjetaAlumno' isn't defined for the type '_MyHomePageState'.
+
+4. Errores de sintaxis:
+Expected to find ';'.
+Expected an identifier.
+Unexpected text ';'.
+
+5. Error de declaración de clase:
+Classes can't be declared inside other classes. Try moving the class to the top-level.
 
--- lib/main.dart
+++ lib/main.dart
@@ -50,17 +50,83 @@ class Alumno {
   }
 }
 
+final List<Alumno> alumnosIniciales = ([
+  const Alumno(
+    id: '1',
+    nombre: 'José Avilés',
+    carnet: '20245984',
+    asistencia: false,
+  ),
+  const Alumno(
+    id: '2',
+    nombre: 'Roberto Rosales',
+    carnet: '20245894',
+    asistencia: false,
+  ),
+  const Alumno(
+    id: '3',
+    nombre: 'Samuel Merino',
+    carnet: '20245356',
+    asistencia: false,
+  ),
+  const Alumno(
+    id: '4',
+    nombre: 'Antonio Pineda',
+    carnet: '20245653',
+    asistencia: false,
+  ),
+  const Alumno(
+    id: '5',
+    nombre: 'Carlos Cruz',
+    carnet: '20245535',
+    asistencia: false,
+  ),
+  const Alumno(
+    id: '6',
+    nombre: 'Antonio Méndez',
+    carnet: '20245355',
+    asistencia: false,
+  ),
+  const Alumno(
+    id: '7',
+    nombre: 'Adrián López',
+    carnet: '20245259',
+    asistencia: false,
+  ),
+  const Alumno(
+    id: '8',
+    nombre: 'Samuel Pimentel',
+    carnet: '20245952',
+    asistencia: false,
+  ),
+  const Alumno(
+    id: '9',
+    nombre: 'Diego Viscarra',
+    carnet: '20245300',
+    asistencia: false,
+  ),
+  const Alumno(
+    id: '10',
+    nombre: 'Alejandro Pineda',
+    carnet: '20245030',
+    asistencia: false,
+  ),
+  const Alumno(
+    id: '11',
+    nombre: 'Carlos Llanes',
+    carnet: '20245303',
+    asistencia: false,
+  ),
+  const Alumno(
+    id: '12',
+    nombre: 'Samuel Cornejo',
+    carnet: '20245033',
+    asistencia: false,
+  ),
+]);
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -69,80 +135,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Alumno> alumnos = ([
-    const Alumno(
-      id: '1',
-      nombre: 'José Avilés',
-      carnet: '20245984',
-      asistencia: false,
-    ),
-    const Alumno(
-      id: '2',
-      nombre: 'Roberto Rosales',
-      carnet: '20245894',
-      asistencia: false,
-    ),
-    const Alumno(
-      id: '3',
-      nombre: 'Samuel Merino',
-      carnet: '20245356',
-      asistencia: false,
-    ),
-    const Alumno(
-      id: '4',
-      nombre: 'Antonio Pineda',
-      carnet: '20245653',
-      asistencia: false,
-    ),
-    const Alumno(
-      id: '5',
-      nombre: 'Carlos Cruz',
-      carnet: '20245535',
-      asistencia: false,
-    ),
-    const Alumno(
-      id: '6',
-      nombre: 'Antonio Méndez',
-      carnet: '20245355',
-      asistencia: false,
-    ),
-    const Alumno(
-      id: '7',
-      nombre: 'Adrián López',
-      carnet: '20245259',
-      asistencia: false,
-    ),
-    const Alumno(
-      id: '8',
-      nombre: 'Samuel Pimentel',
-      carnet: '20245952',
-      asistencia: false,
-    ),
-    const Alumno(
-      id: '9',
-      nombre: 'Diego Viscarra',
-      carnet: '20245300',
-      asistencia: false,
-    ),
-    const Alumno(
-      id: '10',
-      nombre: 'Alejandro Pineda',
-      carnet: '20245030',
-      asistencia: false,
-    ),
-    const Alumno(
-      id: '11',
-      nombre: 'Carlos Llanes',
-      carnet: '20245303',
-      asistencia: false,
-    ),
-    const Alumno(
-      id: '12',
-      nombre: 'Samuel Cornejo',
-      carnet: '20245033',
-      asistencia: false,
-    ),
-  ]);
+  late List<Alumno> alumnos;
+
+  @override
+  void initState() {
+    super.initState();
+    alumnos = List.of(alumnosIniciales);
+  }
+
+  int get _presentes => alumnos.where((a) => a.asistencia).length;
 
   void marcarTodosPresentes() {
     setState(() {
@@ -154,32 +155,37 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void marcarTodosAusentes() {
     setState(() {
-      for (int i = 0; i < alumnos.length; i++) {
-        alumnos[i] = alumnos[i].copyWith(asistencia: false);
-      }
+      alumnos = List.of(alumnosIniciales);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Column(
         children: [
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            color: Theme.of(context).colorScheme.primaryContainer,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Asistencia',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Presentes: $_presentes',
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.all(12),
             child: Row(
@@ -210,80 +216,94 @@ class _MyHomePageState extends State<MyHomePage> {
               itemCount: alumnos.length,
               itemBuilder: (context, index) {
                 final alumno = alumnos[index];
-                final colorFondo = alumno.asistencia
-                    ? Colors.green.shade50
-                    : Colors.red.shade50;
-                final colorBorde = alumno.asistencia
-                    ? Colors.green
-                    : Colors.red;
-
-                return Container(
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 12,
-                    vertical: 8,
-                  ),
-                  child: Card(
-                    color: colorFondo,
-                    elevation: 4,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                      side: BorderSide(color: colorBorde, width: 1.5),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Row(
-                        children: [
-                          CircleAvatar(
-                            backgroundColor: colorBorde,
-                            child: Text(
-                              alumno.nombre.substring(0, 1),
-                              style: const TextStyle(color: Colors.white),
-                            ),
-                          ),
-                          const SizedBox(width: 12),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  alumno.nombre,
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18,
-                                  ),
-                                ),
-                                const SizedBox(height: 4),
-                                Text('Carnet: ${alumno.carnet}'),
-                                const SizedBox(height: 6),
-                                Text(
-                                  alumno.asistencia ? 'Presente' : 'Ausente',
-                                  style: TextStyle(
-                                    color: colorBorde,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Switch(
-                            value: alumno.asistencia,
-                            onChanged: (bool value) {
-                              setState(() {
-                                alumnos[index] = alumno.copyWith(
-                                  asistencia: value,
-                                );
-                              });
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                return TarjetaAlumno(
+                  key: ValueKey(alumno.id),
+                  alumno: alumno,
+                  onCambiar: (bool value) {
+                    setState(() {
+                      alumnos[index] = alumno.copyWith(asistencia: value);
+                    });
+                  },
                 );
               },
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class TarjetaAlumno extends StatelessWidget {
+  final Alumno alumno;
+  final ValueChanged<bool> onCambiar;
+
+  const TarjetaAlumno({
+    super.key,
+    required this.alumno,
+    required this.onCambiar,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colorFondo = alumno.asistencia
+        ? Colors.green.shade50
+        : Colors.red.shade50;
+    final colorBorde = alumno.asistencia ? Colors.green : Colors.red;
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      child: Card(
+        color: colorFondo,
+        elevation: 4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+          side: BorderSide(color: colorBorde, width: 1.5),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundColor: colorBorde,
+                child: Text(
+                  alumno.nombre.substring(0, 1),
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(width: 12),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      alumno.nombre,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text('Carnet: ${alumno.carnet}'),
+                    const SizedBox(height: 6),
+                    Text(
+                      alumno.asistencia ? 'Presente' : 'Ausente',
+                      style: TextStyle(
+                        color: colorBorde,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Switch(
+                value: alumno.asistencia,
+                onChanged: (bool value) {
+                  onCambiar(value);
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
